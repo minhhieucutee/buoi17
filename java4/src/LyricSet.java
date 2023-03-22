aa8d66fbf343b69467f986897598b28bfa885318
Hoàng Minh Hiếu <111435941+minhhieucutee@users.noreply.github.com>
2023-03-22 01:23:34 -0700

    Add files via upload

---
+++ java4/src/LyricSet.java
@@ -0,0 +1,17 @@
+ import java.util.*;
+
+public class LyricSet {
+    private static final String[] lyric = {"you ","Say","it","best","when","you","say","nothing","at","all"};
+    public static void main (String [] args){
+        Set words = new HashSet();
+        for(String w : lyric)
+            words.add(w);
+        for (Object o : words)
+            System.out.println( o +" ");
+        System.out.println("\n----------");
+
+        System.out.println("Contains [you] ? :" + words.contains("you"));
+        System.out.println(" Contains [me] ? :" + words.contains("me"));
+
+    }
+}
---
+++ java4/src/LyricWord.java
@@ -0,0 +1,26 @@
+import java.io.ObjectInput;
+import java.util.*;
+
+public class LyricWord {
+    private static final String [] lyric = {"you","say","it","best","when","you","say","nothing","at","all"};
+    public static void main (String []args){
+        List words = new ArrayList();
+        for (String w : lyric)
+            words.add(w);
+        for (Object o : words)
+            System.out.println( o + " ");
+        System.out.println("\n--------------");
+
+        System.out.println("Contains [you]? : " + words.contains("you"));
+        System.out.println("Contains[me] ?  :"  + words.contains("me")) ;
+        System.out.println("Where's [say] ? :" + words.indexOf("say"));
+        System.out.println("Where's the last [say]?:"  + words.lastIndexOf("Say"));
+
+        Collection.sort (words);
+        for (Object o : words) {
+            System.out.print(o + " ");
+        }
+
+    }
+
+}
